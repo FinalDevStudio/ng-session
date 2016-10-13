458971a16bd7a978330d3198b597805f8ac8e6bb
Santiago G. Marín <santiago@finaldevstudio.com>
2016-10-13 12:09:51 -0300

    Updated unit tests

--- test/unit/ng-session.spec.js
+++ test/unit/ng-session.spec.js
@@ -16,7 +16,8 @@ describe('The ngSession service', function () {
           if (sess) {
             return [200, {
               id: 1,
-              name: 'John Tester'
+              name: 'John Tester',
+              roles: 'user'
             }];
           }
 
@@ -57,11 +58,11 @@ describe('The ngSession service', function () {
     it('Should update the session', function (done) {
       $session.update()
 
-      .then(function () {
+      .then(() => {
         done(new Error('Success callback shouldn\'t be called!'));
       })
 
-      .catch(function (res) {
+      .catch((res) => {
         expect(res).to.be.an('object');
         expect(res.status).to.equal(401);
         expect(res.data).to.be.empty;
@@ -81,11 +82,11 @@ describe('The ngSession service', function () {
 
       $session.signIn(data)
 
-      .then(function () {
+      .then(() => {
         done(new Error('User should not be signed in!\n'));
       })
 
-      .catch(function (res) {
+      .catch((res) => {
         expect(res).to.be.an('object');
         expect(res.status).to.equal(403);
 
@@ -103,7 +104,7 @@ describe('The ngSession service', function () {
 
       $session.signIn(data)
 
-      .then(function (res) {
+      .then((res) => {
         expect(res).to.be.an('object');
         expect(res.status).to.equal(200);
         expect($session.user()).to.not.be.empty;
@@ -113,7 +114,7 @@ describe('The ngSession service', function () {
         done();
       })
 
-      .catch(function (res) {
+      .catch((res) => {
         done(new Error('User should be signed in! (Got ' + res.status + ')\n'));
       });
 
@@ -123,7 +124,7 @@ describe('The ngSession service', function () {
     it('Should sign a user out', function (done) {
       $session.signOut()
 
-      .then(function (res) {
+      .then((res) => {
         expect(res).to.be.an('object');
         expect(res.status).to.equal(204);
         expect($session.user()).to.be.null;
@@ -131,7 +132,7 @@ describe('The ngSession service', function () {
         done();
       })
 
-      .catch(function (res) {
+      .catch((res) => {
         done(new Error('User should be signed out! (Got ' + res.status + ')\n'));
       });
 
@@ -150,6 +151,85 @@ describe('The ngSession service', function () {
       $session.del('test', 1);
 
       expect($session.get('test')).to.be.empty;
+    });
+  });
+
+  describe('Single session role', function () {
+    it('Should assign role', function () {
+      $session.set('user', {
+        name: 'Carl User',
+        roles: 'user'
+      });
+
+      expect($session.user()).to.not.be.empty;
+      expect($session.user('name')).to.be.a('string');
+      expect($session.user('roles')).to.be.a('string');
+    });
+
+    it('Should match role against current session value', function () {
+      expect($session.hasRole('user')).to.be.true;
+      expect($session.hasRole('user', true)).to.be.true;
+    });
+
+    it('Should not match role against current session value', function () {
+      expect($session.hasRole('admin')).to.be.false;
+      expect($session.hasRole('admin', true)).to.be.false;
+    });
+
+    it('Should match the roles against current session value', function () {
+      expect($session.hasRole(['user'])).to.be.true;
+      expect($session.hasRole(['user'], true)).to.be.true;
+      expect($session.hasRole(['user', 'admin'])).to.be.true;
+    });
+
+    it('Should not match the roles against current session value', function () {
+      expect($session.hasRole(['admin'])).to.be.false;
+      expect($session.hasRole(['admin'], true)).to.be.false;
+      expect($session.hasRole(['user', 'admin'], true)).to.be.false;
+    });
+  });
+
+  describe('Multiple session roles', function () {
+    it('Should assign admin and manager roles', function () {
+      $session.set('user', {
+        name: 'Stephanie Manager',
+        roles: ['admin', 'manager']
+      });
+
+      expect($session.user()).to.not.be.empty;
+      expect($session.user('name')).to.be.a('string');
+      expect($session.user('roles')).to.be.an('array');
+    });
+
+    it('Should match role against current session value', function () {
+      expect($session.hasRole('manager')).to.be.true;
+      expect($session.hasRole('manager', true)).to.be.true;
+      expect($session.hasRole('admin')).to.be.true;
+      expect($session.hasRole('admin', true)).to.be.true;
+    });
+
+    it('Should not match role against current session value', function () {
+      expect($session.hasRole('user')).to.be.false;
+      expect($session.hasRole('user', true)).to.be.false;
+    });
+
+    it('Should match the roles against current session value', function () {
+      expect($session.hasRole(['admin'])).to.be.true;
+      expect($session.hasRole(['admin', 'user'])).to.be.true;
+      expect($session.hasRole(['admin'], true)).to.be.true;
+      expect($session.hasRole(['manager'])).to.be.true;
+      expect($session.hasRole(['manager', 'user'])).to.be.true;
+      expect($session.hasRole(['manager'], true)).to.be.true;
+      expect($session.hasRole(['admin', 'manager'])).to.be.true;
+      expect($session.hasRole(['admin', 'manager', 'user'])).to.be.true;
+      expect($session.hasRole(['admin', 'manager'], true)).to.be.true;
+    });
+
+    it('Should not match the roles against current session value', function () {
+      expect($session.hasRole(['user'])).to.be.false;
+      expect($session.hasRole(['user'], true)).to.be.false;
+      expect($session.hasRole(['user', 'admin'], true)).to.be.false;
+      expect($session.hasRole(['user', 'admin', 'manager'], true)).to.be.false;
     });
   });
 });
